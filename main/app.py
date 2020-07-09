14accd0f0371dcb6f92eea477c34a55c11e5713b
Leonard Pahlke <58054077+leonardpahlke@users.noreply.github.com>
2020-07-09 10:34:15 +0200

    Update app.py

--- main/app.py
+++ main/app.py
@@ -13,23 +13,23 @@ from main.Util.UtilRDS import *
 
 
 def lambda_handler(event, context):
-    # lambda_response = DEFAULT_BAD_RESPONSE
-    #
-    # try:
-    #     # get information from request
-    #     request_method = event[METHOD]
-    #     request_option = event[QUERY_STRING_PARAMS][OPTION]
-    #     request_message = event[QUERY_STRING_PARAMS][MESSAGE]
-    #
-    #     # find the requested method-action
-    #     for method_action in method_actions:
-    #         if (request_method == method_action.http_method) and (request_option == method_action.option):
-    #             # execute method-action
-    #             lambda_response = method_action.execute(request_message)
-    #             break
-    # except Exception as e:
-    #     lambda_response[BODY][LOG] = e
+    lambda_response = DEFAULT_BAD_RESPONSE
+    
+    try:
+        # get information from request
+        request_method = event[METHOD]
+        request_option = event[QUERY_STRING_PARAMS][OPTION]
+        request_message = event[QUERY_STRING_PARAMS][MESSAGE]
+    
+        # find the requested method-action
+        for method_action in method_actions:
+            if (request_method == method_action.http_method) and (request_option == method_action.option):
+                # execute method-action
+                lambda_response = method_action.execute(request_message)
+                break
+    except Exception as e:
+        lambda_response[BODY][LOG] = e
 
-    RDSDatabase().run_query(CREATE_TABLE_QUERY)
+    # RDSDatabase().run_query(CREATE_TABLE_QUERY)
 
     return ''  # lambda_response
